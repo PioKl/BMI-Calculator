ff215e49edb6e7d40d6474e89324991c3157be71
PioKl <kosiu342@gmail.com>
2024-07-10 21:34:05 +0200

    Add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./style/App.scss";
 import Hero from "./components/Hero";
+import ResultMeans from "./components/ResultMeans";
+import HealthySolutions from "./components/HealthySolutions";
+import LimitationsOfBmi from "./components/LimitationsOfBmi";
 
 function App() {
   return (
     <>
       <Hero />
+      <ResultMeans />
+      <HealthySolutions />
+      <LimitationsOfBmi />
     </>
   );
 }
